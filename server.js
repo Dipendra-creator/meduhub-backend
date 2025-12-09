c3b7a52b40509ba94fc6655bae6e778b9af4538a
Dipendra Bhardwaj <dipendra.bhardwaj@bytescare.com>
2025-12-09 14:11:47 +0530

    cors

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
